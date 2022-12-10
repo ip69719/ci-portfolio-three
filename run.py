59a0e47ce56413bc07b05fbcab4edde042e8fb9d
ip69719 <ipeters@mail.com>
2022-12-10 15:45:20 +0000

    feat: Add validation to check that date provided is in expected format

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Import datetime and date classes from datetime module
+required to manipulate dates.
+"""
+from datetime import datetime
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -46,7 +52,21 @@ def get_date():
     print("Date should be in YYYY-MM-DD format.")
 
     user_date_str = input("Enter date here: ")
-    print(user_date_str)
+    validate_date(user_date_str)
+
+
+def validate_date(date_str):
+    """
+    Function checks if date input by User matches the expected format
+    and raises ValueError if it does not.
+    """
+    print("Hello from validate_date function")
+
+    try:
+        if date_str != datetime.strptime(date_str, "%Y-%m-%d").strftime('%Y-%m-%d'):
+            raise ValueError()
+    except ValueError:
+        print("Invalid data: Date should be in YYYY-MM-DD format, please try again.\n")
 
 
 get_date()
